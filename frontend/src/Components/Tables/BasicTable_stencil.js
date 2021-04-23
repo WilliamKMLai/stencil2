0d60814ab2a9daa7f874e79b7cc6cfa3720ae9af
WilliamKMLai <kai.ming.lai@gmail.com>
2021-04-23 15:48:40 -0400

    Update Table to provide default filtering capability

--- frontend/src/Components/Tables/BasicTable_stencil.js
+++ frontend/src/Components/Tables/BasicTable_stencil.js
@@ -2,36 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { CardContent } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-
 // Table components
-import { DataGrid } from "@material-ui/data-grid";
-
-// Component styles
-const useStyles = makeStyles({
-  fullList: {
-    width: "auto",
-    height: "auto"
-  }
-});
+import MUIDataTable from "mui-datatables";
 
 function BasicTable(props) {
-    const classes = useStyles(props);
-
     if (props.tableData === undefined ){
         return "No table data detected";
     }
 
-    //console.log(props);
-    //console.log(props.tableData);
-    //console.log(props.tableData.columns);
-    //console.log(props.tableData.rows);
+    const options = {
+        filterType: "dropdown",
+        elevation: 1,
+        selectableRows: "none",
+        responsive: "standard",
+        print: false,
+        downloadOptions: {
+          filename: "table.csv",
+          separator: ",",
+        },
+        rowsPerPageOptions: [10, 15, 50, 100],
+        //tableBodyHeight: 'auto',
+        tableBodyHeight: props.height + 'px'
+      };
+
     return (
-      <CardContent className={classes.fullList}>
-        <div style={{ height: props.height, width: props.width }}>
-          <DataGrid rows={props.tableData.rows} columns={props.tableData.columns} pageSize={5} />
+      <CardContent>
+        <div>
+          <MUIDataTable columns={props.tableData["columns"]} data={props.tableData["rows"]} options={options}/>
         </div>
       </CardContent>
+      //<MUIDataTable
+      //  data={tableData}
+      ///>
     );
 }
 
