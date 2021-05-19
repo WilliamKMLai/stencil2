e7600ff41a4e0c696c8fc63cd9627382670ca7a6
Qi Sun <qisun@RS-BTXM1YQS24.cornell.edu>
2021-05-19 08:54:05 -0400

    solve problem  of flashing back to login when change project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -234,7 +234,7 @@ class App extends Component {
               <DataProvider value={appData}>
                 <Navbar uid={this.state.uid} role={this.state.role} currentProj={this.state.currentProject} searchOptions={this.state.allLibraryList}  defaultText="Search by experiment ID" handle="getLib"  />
                 <Switch>
-                  <Route exact path="/project/:proj_id" component={this.state.login? (HomePage):(LoginPage)} />
+                  <Route exact path="/project/:proj_id" component={this.state.login? (HomePage):(null)} />
                   <Route exact path="/login" component={LoginPage} />
                   <Route exact path="/admin" component={this.state.login? (AdminPage):(LoginPage)} />
                   <Route exact path="/account" component={this.state.login? (AccountPage):(LoginPage)} />
